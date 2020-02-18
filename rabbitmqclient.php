7585fce36a8a68468b75d3bfa23db953eebda7d4
Dhruv55 <43227604+Dhruv55@users.noreply.github.com>
2020-02-18 17:14:38 -0500

    Update rabbitmqclient.php

--- rabbitmqclient.php
+++ rabbitmqclient.php
@@ -1,7 +1,4 @@
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
 
 $client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
 if(isset($argv[1])){
