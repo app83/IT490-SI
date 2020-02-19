d4ddde2bac3a754875f633964e406d440b3e3a91
app83 <45634002+app83@users.noreply.github.com>
2020-02-18 19:43:53 -0500

    Update RabbitMQClientSample.php

--- RabbitMQClientSample.php
+++ RabbitMQClientSample.php
@@ -1,21 +1,30 @@
+#!/usr/bin/php
 <?php
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
-if(isset($argv[1])){
-	$msg = $argv[1];
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
 }
-else{
-	$msg = array("message"=>"test message", "type"=>"echo");
+else
+{
+  $msg = "test message";
 }
 
-$response = $client->send_request($msg);
+$request = array();
+$request['type'] = "Login";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
 
-echo "client received response: " . PHP_EOL;
+echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
 
-if(isset($argv[0]))
-echo $argv[0] . " END".PHP_EOL;
+echo $argv[0]." END".PHP_EOL;
+?>
