9858f901308e1496aa195d250656ce911227bd59
np624 <np624@njit.edu>
2020-03-03 18:00:28 -0500

    updated the root path

--- frontend/example/front_end/app.py
+++ frontend/example/front_end/app.py
@@ -1,9 +1,7 @@
-from flask import Flask  
-app = Flask(__name__) 
-   
+from flask import Flask, render_template
+app = Flask(__name__)  
  @app.route('/')
-
-def index():
+def login():
         return render_template('login.html')     
 if __name__ == '__main__':  
    app.run(debug = True) 
