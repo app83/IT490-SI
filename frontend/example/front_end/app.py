fc1f910787afb721b26590ce556f130324dc2f01
np624 <np624@njit.edu>
2020-03-02 19:18:51 -0500

    added a flask application setup folder

--- frontend/example/front_end/app.py
+++ frontend/example/front_end/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-        return 'Hello, World!'
+# @app.route('/')
+# def hello_world():
+#         return 'Hello, World!'
+from flask import *  
+app = Flask(__name__)  
+ 
+@app.route('/')  
+def message():  
+      return render_template('login.html')  
+if __name__ == '__main__':  
+   app.run(debug = True) 
