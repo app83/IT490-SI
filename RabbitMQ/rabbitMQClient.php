3eefde2597fea84d9af9d55b2c0a26da76f39950
Ami Patel <app83@njit.edu>
2020-03-03 18:16:04 -0500

    Resolved merge conflict by incorporating both suggestions.

--- RabbitMQ/rabbitMQClient.php
+++ RabbitMQ/rabbitMQClient.php
@@ -1,12 +1,12 @@
+#!/usr/bin/php
 <?php
-
-    require_once('../RabbitMQ/path.inc');
-    require_once('../RabbitMQ/get_host_info.inc');
-    require_once('../RabbitMQ/rabbitMQLib.inc');
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
     //  creates rabbitMq client instance for Database server
     function createClientForDb($request){
-        $client = new rabbitMQClient("../RabbitMQ/testRabbitMQ.ini", "testServer");
+        $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
         
         if(isset($argv[1])){
             $msg = $argv[1];
