c337a492457a2eb1ff9f094c44bb90124a9e850f
np624 <np624@njit.edu>
2020-03-03 14:56:15 -0500

    updated the root path

--- frontend/example/front_end/app.py
+++ frontend/example/front_end/app.py
@@ -4,8 +4,8 @@
 # @app.route('/')
 # def hello_world():
 #         return 'Hello, World!'
-from flask import Flask, render_template
-app = Flask(__name__, root_path='ui_template/') 
+from flask import Flask
+app = Flask(__name__) 
    
  @app.route('/')
 # render home page 
