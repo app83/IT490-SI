f7eb8ac3a56412b12aff8f6f0c72878677fe8293
pp733 <61157482+pp733@users.noreply.github.com>
2020-03-03 18:21:49 -0500

    Add files via upload

---
+++ dmz/RabbitMQ/rabbitMQClient.php
@@ -0,0 +1,25 @@
+// This file will create rabbitMQ Client for DB Server
+
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+    //  creates rabbitMq client instance for Database server
+    function createClientForDb($request){
+        $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+        
+        if(isset($argv[1])){
+            $msg = $argv[1];
+            
+        } else{
+            $msg = "client";
+        }
+        
+        $response = $client->send_request($request);
+        
+        return $response;
+    }
+    
+?>
