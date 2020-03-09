c329f6d11a70e8286536b5034b1546c0bee4f8b5
pp733 <61157482+pp733@users.noreply.github.com>
2020-03-09 18:14:02 -0400

    Update dbListener.php

--- database/php/dbListener.php
+++ database/php/dbListener.php
@@ -3,11 +3,11 @@
 
 
 
-    require_once('../RabbitMQ/path.inc');
+    require_once('../path.inc');
 
-    require_once('../RabbitMQ/get_host_info.inc');
+    require_once('../get_host_info.inc');
 
-    require_once('../RabbitMQ/rabbitMQLib.inc'); 
+    require_once('../rabbitMQLib.inc'); 
 
 
 
@@ -119,7 +119,7 @@
 
 
 
-    $server = new rabbitMQServer("../RabbitMQ/testRabbitMQ.ini","testServer");
+    $server = new rabbitMQServer("../testRabbitMQ.ini","testServer");
 
 
 
