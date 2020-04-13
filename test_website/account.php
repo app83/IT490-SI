74b26a0115fe197ea6888305263f3fdcff5f582b
Dhruv55 <43227604+Dhruv55@users.noreply.github.com>
2020-04-13 19:24:10 -0400

    Update account.php

--- test_website/account.php
+++ test_website/account.php
@@ -9,7 +9,7 @@ Change username/project/password to yours as well, if different.
 $dbname = 'IT490';
 $dbuser = 'test';
 $dbpass = 'password';
-$dbhost = '192.168.1.50';
+$dbhost = '25.2.139.140';
 
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
