dd02ecc108b3bbd5944e42c61a39d9cc2d553b3f
Krishan Patel <50147772+kpatel-1@users.noreply.github.com>
2020-04-21 19:51:17 -0400

    Update receive.php

--- RabbitMQ/receive.php
+++ RabbitMQ/receive.php
@@ -4,10 +4,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'admin', 'admin');
+$connection = new AMQPStreamConnection('25.5.178.51', 5672, 'admin', 'admin');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+#$channel->queue_declare('hello', false, false, false, false);
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
@@ -15,7 +15,8 @@ $callback = function ($msg) {
     echo ' [x] Received ', $msg->body, "\n";
 };
 
-$channel->basic_consume('hello', '', false, true, false, false, $callback);
+$channel->basic_consume('q1', 'e1', false, true, false, false, $callback);
+
 
 while ($channel->is_consuming()) {
     $channel->wait();
