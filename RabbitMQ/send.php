a3b606105d9116ba85e7a1b4ccd8e623074fceca
pp733 <61157482+pp733@users.noreply.github.com>
2020-04-20 22:19:53 -0400

    Update send.php

--- RabbitMQ/send.php
+++ RabbitMQ/send.php
@@ -5,13 +5,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'admin', 'admin');
+//$connection = new AMQPStreamConnection('localhost', 5672, 'admin', 'admin');
+$connection = new AMQPStreamConnection('25.5.178.51', 5672, 'admin', 'admin');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+// We dont need that
+//$channel->queue_declare('hello', false, false, false, false);
 
 $msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, 'e1', 'hello');   // ( ,exchange, routing key)
 
 echo " [x] Sent 'Hello World!'\n";
 
