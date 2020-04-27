994f39db26578763e0bc9b2713062f3b879bff7c
Krishan Patel <50147772+kpatel-1@users.noreply.github.com>
2020-04-27 11:54:41 -0400

    Update receive.php

--- RabbitMQ/receive.php
+++ RabbitMQ/receive.php
@@ -14,14 +14,18 @@ echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 $callback = function ($msg) {
     echo ' [x] Received ', $msg->body, "\n";
+    
+    //Converts msg to string & decodes
     $aMsg = $msg->body;
     $newMsg = "[{$aMsg}]";
     $arr = json_decode($newMsg, true);
 
+    //Stores information into vars
     $name = $arr[0]["name"];
     $email = $arr[0]["email"];
     $pass = $arr[0]["pass"];
 
+    //Prints vars
     echo 'name: ',$name, "\n";
     echo 'email: ',$email, "\n";
     echo 'pass: ',$pass, "\n";
