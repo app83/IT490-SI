77d3157b061ae7b5b4ebc5d2f186b3d682c2faad
Krishan Patel <50147772+kpatel-1@users.noreply.github.com>
2020-04-29 18:46:25 -0400

    Update receive.php

--- RabbitMQ/receive.php
+++ RabbitMQ/receive.php
@@ -1,10 +1,10 @@
 #!/usr/bin/php
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('25.5.178.51', 5672, 'admin', 'admin');
+
+$connection = new AMQPStreamConnection('25.143.171.73', 5672, 'admin', 'admin');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
@@ -12,7 +12,19 @@ $channel->queue_declare('hello', false, false, false, false);
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 
+
 $callback = function ($msg) {
+    
+    //Database Connection
+	$dbname = 'IT490A';
+	$dbuser = 'test1';
+	$dbpass = 'password';
+	$dbhost = '25.5.139.140';
+
+	$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+	mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
+
+
     echo ' [x] Message Received ', $msg->body, "\n";
     
     //Converts msg to string & decodes
@@ -31,10 +43,30 @@ $callback = function ($msg) {
     echo 'email: ',$email, "\n";
     echo 'pass: ',$pass, "\n";
     echo 'type: ',$type, "\n";
+
+
+    //Insert account into users table
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name','$email','$pass')";
+    ($t = mysqli_query( $connect, $sql )) or die(mysqli_error($connect));
 };
+
+
 
 $channel->basic_consume('q1', 'e1', false, true, false, false, $callback);
 
+
+while ($channel->is_consuming()) {
+    $channel->wait();
+}
+
+
+$ar = array($msg);
+
+
+
+$channel->close();
+$connection->close();
+?>
 
 while ($channel->is_consuming()) {
     $channel->wait();
