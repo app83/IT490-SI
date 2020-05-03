eeafbb9c7b4d5d902f7a0a64942b479b4dd23ab7
np624 <60897602+np624@users.noreply.github.com>
2020-05-03 02:03:45 -0400

    Update bmi2.php

--- test_website/final/bmi2.php
+++ test_website/final/bmi2.php
@@ -21,34 +21,28 @@ $email = $_SESSION['email'];
 
 ?>
 
-
 <!DOCTYPE html>
 <html>
-
-<head>
-    <style>
-        table {
-            font-family: arial, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        td,
-        th {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-
-        tr:nth-child(even) {
-            background-color: #dddddd;
-        }
-    </style>
-	<link rel= "stylesheet" type="text/css" href="css/style.css">
-</head>
-
-<body>
-	<div class= "main">
+    <head>
+        <link rel= "stylesheet" type="text/css" href="css/style.css">
+        <style>
+            #outer{background:#fff;text-align:center;}
+            #cover{border:2px solid #111;border-radius:1px;box-shadow:inset 0 0 13px #888;padding:7px 0}
+            .main{table-layout:fixed;width:100%;border:0;border-collapse:collapse;margin:0 auto;}
+            .main td{padding:0 8px;vertical-align:middle;text-align:left;border:0;font:500 11px arial}
+            .main input{width:96%;border:1px solid #ccc;margin:2px 0;padding:0 2%;height:17px;text-align:right;font:500 11px arial;background:none;}td.ac {text-align:center}
+            .main select{width:100%;border:1px solid #ccc;margin:2px 0;background:#fff;height:18px;font:500 11px arial}.main button{width:100%;font:600 12px arial;margin:2px 0;}.w60{width:60%}.w40{width:40%}
+            tr {
+                height: 60px;
+            }
+       </style>
+        <title>BMI Calculator</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+	  
+        
+    </head>
+    <body>
+	 <div class= "main" >
       <nav>
         <div class= "logo">
           <img src= "images/healtholic.jpeg" alt="" width="150" height="150">
@@ -59,50 +53,83 @@ $email = $_SESSION['email'];
 	 <li><form action="logout.php"><input type="submit" value="Logout"></form></li>	
         </ul>
       </nav>
+    </div>  
+	<div class="bg-text" style="top: 70%;">
+        <div id=outer>
+            <div id=cover>
+                <form name=fn>
+                    <table class=main>
+                        <col class=w60><col class=w60>
+                        <tr><td>Measuring System<td>
+                            <select id=msm onchange=msystem();>
+                                <option value=metric>Metric (Kgs, Cms)
+                                    <option value=us>US (lbs, inches)
+                            </select>
+                        <tr><td>Height<span id=thm> (Cms Or inches)</span>
+                            <td><input id=hm><tr><td>Weight <span id=twm> (Kgs Or lbs)</span>
+                            <td><input id=wm>
+                        <tr><td><button type=reset>Reset</button>
+                            <td><button type=button onclick='bmass();'>Submit</button>
+                        <tr><td>Body Mass Index (BMI)
+                            <td><input id=bmi class=op>
+                        <tr><td>Normal BMI range
+                            <td><input class=op id=nbmi value='18.5-25 kg/m2'>
+                        <tr><td colspan=2 class=ac><br>The calculations are based on WHO recommendations.
+                    </table>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="bg-output"> </div> 
- <div class="bg-text">
-    <h3>Nutrition Search</h3>
+        <script>
+            var $=function( id ){
+                return document.getElementById(id);
+            };
+                function msystem(){
+                    if($('msw').value=='metric'){
+                        $('thw').innerHTML=' (Cms)';
+                    }
+                    else{
+                        $('thw').innerHTML=' (inches)';
+                        };
 
-    <input onkeyup="search()"" type=" text" id="myText" value="">
-    <div id="searchresults"></div>
-    <p id="demo"></p>
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-    <script>
+                        if($('msm').value=='metric'){
+                            $('thm').innerHTML=' (Cms)';$('twm').innerHTML=' (Kgs)';
+                        }
+                        else{
+                            $('thm').innerHTML=' (inches)';$('twm').innerHTML=' (lbs)';
+                        };
 
-        var filterData = []
+                        if($('msf').value=='metric'){
+                            $('thf').innerHTML=' (Cms)';
+                            $('tneck').innerHTML=' (Cms)';
+                            $('twaist').innerHTML=' (Cms)';
+                            $('thips').innerHTML=' (Cms)';
+                        }
+                        else{
+                            $('thf').innerHTML=' (inches)';
+                            $('tneck').innerHTML=' (inches)';
+                            $('twaist').innerHTML=' (inches)';
+                            $('thips').innerHTML=' (inches)';
+                        }
+                    }
+                function bmass (){
+                    var ms=$('msm').value;
+                    var height=$('hm').value;
+                    var weight=$('wm').value;
+                    if(height==null || height.length==0 || weight==null || weight.length==0 ){
+                        $('bmi').value="Pl. enter data.";
+                    }
+                    else{
+                        $('bmi').value="";
+                    }
 
-        function search() {
-            var obj, dbParam, xmlhttp, myObj, x, txt = "";
-            obj = { table: "customers", limit: 20 };
-
-            var searchValue = document.getElementById('myText').value
-            axios.get(`https://api.edamam.com/api/food-database/parser?nutrition-type=logging&app_id=07d50733&app_key=80fcb49b500737827a9a23f7049653b9&ingr=${searchValue}`)
-                .then(response => {
-                    var resultsData = response.data.hints;
-                    myObj = resultsData
-                    txt += "<table border='1'><tr><th>Food</th><th>Image</th><th>Energy</th><th>Calories</th></tr>"
-                    $.each(resultsData, function (index, value) {
-                        const food = value.food
-                        const brand = food.brand ? food.brand : ''
-                        const label = food.label ? food.label : ''
-    
-                        const nutrients = food.nutrients
-                        const measures = value.measures
-                        console.log(food, measures)
-                        txt += "<tr><td>" + brand + ''+ label + "</td><td>" + `<img style="width:80px" src=${food.image}></img>` + "</td><td>" + Math.round( nutrients.ENERC_KCAL) + ' kcal' + "</td><td>" + 'protein: ' + Math.round(nutrients.PROCNT) + 'g' +'<br>' + 'Carbs: ' + Math.round(nutrients.FAT) +'g'  + '<br>'+ 'Fat: ' + Math.round(nutrients.CHOCDF) + 'g' + "</td></tr>"
-                    });
-                    txt += "</table>"    
-                    document.getElementById("demo").innerHTML = txt;
-                })
-                .catch(error => {
-                    
+                    if(ms=='metric'&&height>0){
+                        $('bmi').value=Math.round(weight/(height*height/10000)*100)/100+" kg/m2 " 
+                    }
+                    else if(ms=='us'&&height>0){
+                        $('bmi').value=Math.round(703*weight/(height*height)*100)/100+" kg/m2 "
+                    }
                 }
-                );
-        }
-    </script>
-</div>
-</body>
-
+        </script>   
+    </body>
 </html>
